b7ac95dfcda8412dd64f307ffaec6645967a85bc
Eric Yeh <spu8673@gmail.com>
2022-06-28 01:21:59 +0800

    feat: 新增洗牌功能 listArray 裡面物件都有兩個，隨機排列後依序將第一個分給每個 .card-back

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,6 +1,34 @@
+// let firstPick;
+// let secondPick;
+// let isPicked = false;
+// let checking = false;
+// let totalCard = 2;
+// /* --gameCheck-- */
+// let interval;
+// let sec = 0;
+// let ten = 0;
+// let appendTen = document.getElementById('ten');
+// let appendSec = document.getElementById('sec');
+/* --Time-- */
+const itemList = ['isHen', 'isDolphin', 'isDucky', 'isJellyfish', 'isSnail', 'isToad'];
+const listArray = itemList.slice(0).concat(itemList);
+// 把物品清單複製一份然後跟原本的合併
+/* --Shuffle--*/
+
+const shuffle = () => {
+  const backs = document.querySelectorAll('.card-back');
+  listArray.sort(() => Math.random() - 0.5);
+  backs.forEach((back) => {
+    back.classList.add(listArray[0]);
+    listArray.shift();
+  });
+};
+
 const cards = document.querySelectorAll('.card');
 cards.forEach((card) => {
   card.addEventListener('click', () => {
     card.classList.toggle('flip');
   });
 });
+
+shuffle();
