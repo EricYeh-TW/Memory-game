ca2a2db3ea90f9eceb07f064100ce23f259252d2
Eric Yeh <spu8673@gmail.com>
2022-06-29 01:05:55 +0800

    fix: 修改圖片 class 的位置 原本圖片 class 放在 card-back 上，使用 DOM 時判斷 card 的 childNode 過程過於冗長，直接修改在 card 之上並且修改 CSS 對圖片進行調整

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -14,32 +14,32 @@ let totalCard = 6;
 const itemList = ['isHen', 'isDolphin', 'isDucky', 'isJellyfish', 'isSnail', 'isToad'];
 const listArray = itemList.slice(0).concat(itemList);
 
-// const reset = () => [null, null, false, false];
+const reset = () => [null, null, false, false];
 
-// const match = (first, second) => {
-//   if (first !== second) {
-//     checking = true;
-//     setTimeout(() => {
-//       first.classList.remove('flip');
-//       second.classList.remove('flip');
-//       [first, second, isPicked, checking] = reset();
-//     }, 600);
-//   } else {
-//     checking = true;
-//     totalCard -= 1;
-//     first.removeEventListener('click', gameCheck);
-//     second.removeEventListener('click', gameCheck);
-//     // checkGameTime(totalCard);
-//     setTimeout(() => {
-//       first.classList.add('match');
-//       second.classList.add('match');
-//       if (document.body.animate) {
-//         // getParticlePosition(first, second);
-//       }
-//       [first, second, isPicked, checking] = reset();
-//     }, 600);
-//   }
-// };
+const match = (first, second) => {
+  if (firstPick.classList.value !== secondPick.classList.value) {
+    checking = true;
+    setTimeout(() => {
+      first.classList.remove('flip');
+      second.classList.remove('flip');
+      [first, second, isPicked, checking] = reset();
+    }, 600);
+  } else {
+    checking = true;
+    totalCard -= 1;
+    first.removeEventListener('click', gameCheck);
+    second.removeEventListener('click', gameCheck);
+    // checkGameTime(totalCard);
+    setTimeout(() => {
+      first.classList.add('match');
+      second.classList.add('match');
+      if (document.body.animate) {
+        // getParticlePosition(first, second);
+      }
+      [first, second, isPicked, checking] = reset();
+    }, 600);
+  }
+};
 
 // eslint-disable-next-line func-names
 const gameCheck = function () {
@@ -53,27 +53,21 @@ const gameCheck = function () {
   this.classList.add('flip');
   if (!isPicked) {
     isPicked = true;
-    firstPick = this.childNodes[3].classList.value;
-    console.log(firstPick);
+    firstPick = this;
   } else {
-    secondPick = this.childNodes[3].classList.value;
-    console.log(firstPick === secondPick);
-    // match(firstPick, secondPick);
+    secondPick = this;
+    match(firstPick, secondPick);
   }
 };
 
-const shuffle = () => {
-  const backs = document.querySelectorAll('.card-back');
+const shuffle = (card) => {
   listArray.sort(() => Math.random() - 0.5);
-  backs.forEach((back) => {
-    back.classList.add(listArray[0]);
-    listArray.shift();
-  });
+  card.classList.add(listArray[0]);
+  listArray.shift();
 };
 
 const cards = document.querySelectorAll('.card');
 cards.forEach((card) => {
+  shuffle(card);
   card.addEventListener('click', gameCheck);
 });
-
-shuffle();
